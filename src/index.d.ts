a466fb200c1b362b2d1529fcfd7e1fcc17e3463b
Pierre-Philippe Prevost <ppprevost@aol.fr>
2019-06-24 17:39:43 +0200

    types add typescript definition

---
+++ src/index.d.ts
@@ -0,0 +1,38 @@
+declare module '@lectra/ld-react-feature-flags' {
+  interface PropsFlag {
+    children?: React.Element;
+    flag: string;
+    renderOn?: (flag: any) => any;
+    fallbackRender?: (flag: any) => any;
+  }
+
+  interface PropsFlagProvider {
+    children: React.Element;
+    user: {
+      key: string;
+    };
+    clientkey?: string;
+    bootstrap?: object;
+    onFlagsChange?: () => any;
+    loadingComponent?: React.Element;
+  }
+
+  export const WithFlags = <P extends object>(key: string) => (
+    ComponentA: any,
+    ComponentB?: React.ComponentType<P>
+  ) => JSX.Element;
+
+  export class Flags extends React.Component<PropsFlag> {
+    public render(): React.Element;
+  }
+
+  // tslint:disable-next-line:max-classes-per-file
+  export class FlagsProvider extends React.Component<
+    PropsFlagProvider,
+    { isFlagsLoaded: boolean }
+  > {
+    public LDReadyEvent(): Promise<any>;
+    public async componentDidMount(): void;
+    public render(): React.Element;
+  }
+}
